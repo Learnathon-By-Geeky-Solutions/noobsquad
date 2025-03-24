0f60d5feb9b846902a07481c11f57cc1d713f33e
AnishRoy50 <royanish5kb2m@gmail.com>
2025-03-25 02:52:53 +0600

    Fixed some errors

--- backend/api/v1/endpoints/chat.py
+++ backend/api/v1/endpoints/chat.py
@@ -49,6 +49,12 @@ async def websocket_endpoint(websocket: WebSocket, user_id: int):
                         "receiver_id": receiver_id,
                         "content": content,
                     }))
+                if user_id in clients:
+                    await clients[user_id].send_text(json.dumps({
+                        "sender_id": user_id,
+                        "receiver_id": receiver_id,
+                        "content": content,
+                    }))
                 else:
                     print(f"⚠️ Receiver {receiver_id} not connected.")
 
