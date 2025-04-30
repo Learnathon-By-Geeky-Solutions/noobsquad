98615a84d1e7ab4a9531a55e5b8b966c2e81eda7
AnishRoy50 <royanish5kb2m@gmail.com>
2025-05-01 00:27:34 +0600

    Last commit

--- backend/cloudinary_config.py
+++
@@ -1,9 +0,0 @@
-import cloudinary
-import cloudinary.uploader
-
-cloudinary.config(
-  cloud_name="dws8lpmua",
-  api_key="595136529363616",
-  api_secret="VwJl7HXcox_1U9qeeN0fVmoL_VY",
-  secure=True
-)
